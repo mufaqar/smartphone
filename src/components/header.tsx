5317ac2e4e41e32902c88e453c17d488c713fd8d
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-12-15 18:30:40 +0500

    updated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,71 +6,13 @@ import { GiCancel } from "react-icons/gi";
 import Image from 'next/image';
 import { FaPhoneAlt, FaEnvelope, FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram } from 'react-icons/fa';
 import { FaLocationDot } from 'react-icons/fa6';
+
+
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    // <section className='sticky top-0 z-50'>
-    //   <motion.header
-    //     initial={{ x: -100, opacity: 0 }}
-    //     animate={{ x: 0, opacity: 1 }}
-    //     transition={{
-    //       delay: 0.2,
-    //       x: { type: "spring", stiffness: 60 },
-    //       opacity: { duration: 1 },
-    //       ease: "easeIn",
-    //       duration: 1,
-    //     }} className="flex items-center justify-between flex-wrap p-6 lg:bg-teal-950 bg-teal-500">
-    //     <nav className='container mx-auto px-4'>
-    //       <div className="lg:absolute flex top-0 flex-shrink-0 text-white mr-6 lg:mr-72 bg-white w-48 skew-x-12 lg:h-full" >
-    //       <Link href="/" className='inline-block'>
-    //         <Image src="/images/logo.avif" alt="logo" width={907} height={285} className='md:w-1/2 w-full' />
-    //       </Link>
-    //       </div>
-    //       <div className="block lg:hidden">
-    //         <button
-    //           onClick={() => setIsOpen(!isOpen)}
-    //           className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
-    //         >
-
-
-    //           <motion.li
-    //             initial={{ scale: 0 }}
-    //             animate={{ rotate: 180, scale: 1 }}
-    //             transition={{
-    //               type: "spring",
-    //               stiffness: 260,
-    //               damping: 20
-    //             }} className={`fill-current text-3xl  ${isOpen ? "block" : "hidden"}`}>
-    //             <GiCancel />
-    //           </motion.li>
-    //           <li
-    //             className={`fill-current text-3xl ${isOpen ? "hidden" : "block"}`}
-    //           >
-    //             <LuMenuSquare />
-    //           </li>
-    //         </button>
-    //       </div>
-    //       <div
-    //         className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? "block" : "hidden"}`}
-    //       >
-    //         <div className="text-sm lg:flex-grow lg:pl-72">
-    //           {data.map((item, idx) => (
-    //             <Link key={idx} href="#" className='block  hover:bg-[#D79800] px-4 py-2 lg:inline-block text-lg lg:mt-0 text-white mr-4 transition duration-300 ease-in-out hover:opacity-100 hover:text-black font-[light]'>
-    //               {item.title}
-    //             </Link>
-    //           ))}
-    //         </div>
-    //         <div>
-    //           <button className='inline-flex items-center bg-blue-600 border-0 py-2 px-4 text-white cursor-pointer hover:bg-[#D79800] hover:text-white transition duration-300 ease-in-out hover:opacity-100 font-[light]'>
-    //             Click Me
-    //           </button>
-    //         </div>
-    //       </div>
-    //     </nav>
-    //   </motion.header>
-    // </section>
-    <header className='bg-white relative'>
+    <header className='bg-white relative md:block hidden'>
       <div className='bg-black py-3'>
         <div className='container mx-auto px-4'>
           <div className='flex justify-between items-center w-2/3 ml-auto'>
@@ -149,9 +91,6 @@ function Header() {
   );
 }
 export default Header
-
-
-
 
 export const data = [
   {
