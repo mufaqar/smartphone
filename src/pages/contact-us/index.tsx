b8b2443f1589035a7f3b41eedc26e4f59e01d346
mufaqar <mufaqar@gmail.com>
2023-12-22 15:39:22 +0500

    email

--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -1,10 +1,40 @@
 import PageBanner from '@/components/page-banner'
 import SeoMeta from '@/components/seo';
 import Link from 'next/link';
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm, SubmitHandler } from "react-hook-form";
+
 import { FaGlobeAfrica, FaMapMarkerAlt, FaPhoneAlt, FaRegEnvelope } from "react-icons/fa";
 
 function Contact_Us() {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        reset,
+        formState: { errors },
+    } = useForm<any>()
+    const [sending, setSending] = useState(false)
+
+    const onSubmit: SubmitHandler<any> = (data: any) => {
+        setSending(true)
+        fetch('/api/email', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json, text/plain, */*',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        }).then((res) => {
+            console.log('Response received');
+            if (res.status === 200) {
+                console.log('Response succeeded!');
+                alert('Message Successfully send.!');
+                reset();
+                setSending(false)
+            }
+        });
+    }
     return (
         <>
             <SeoMeta title="Contact Us | Budget Computers and Kiwi Mobiles" description="Budget Computers and Kiwi Mobiles" url="https://budgetrepaircenter.nz/contact-us" />
@@ -51,52 +81,49 @@ function Contact_Us() {
                             </div>
                         </div>
                         <div className="mx-auto max-w-[700px]">
-                            <form>
-                                <div className="relative mb-6">
-                                    <input type="text"
-                                        className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear peer-focus:text-yellow-500 motion-reduce:transition-none placeholder:opacity-0"
-                                        id="fname"
-                                        name="fname"
-                                        placeholder='First Name' />
-                                    <label
-                                        className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-yellow-500 motion-reduce:transition-none"
-                                        htmlFor="fname">Name
-                                    </label>
-                                </div>
-                                <div className="relative mb-6">
-                                    <input type="email"
-                                        className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear peer-focus:text-yellow-500 motion-reduce:transition-none placeholder:opacity-0"
-                                        id="email"
-                                        name="email"
-                                        placeholder='Email address' />
-                                    <label
-                                        className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-yellow-500 motion-reduce:transition-none"
-                                        htmlFor="email">Email address
-                                    </label>
-                                </div>
-                                <div className="relative mb-6">
-                                    <input type="phone"
-                                        className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear peer-focus:text-yellow-500 motion-reduce:transition-none placeholder:opacity-0"
-                                        id="phone"
-                                        name="phone"
-                                        placeholder='Phone' />
-                                    <label
-                                        className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-yellow-500 motion-reduce:transition-none"
-                                        htmlFor="email">Phone
-                                    </label>
-                                </div>
-                                <div className="relative mb-6">
-                                    <textarea
-                                        className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear motion-reduce:transition-none  placeholder:opacity-0"
-                                        id="exampleFormControlTextarea1" rows={5} placeholder="Your message"></textarea>
-                                    <label htmlFor="exampleFormControlTextarea1"
-                                        className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-yellow-500 motion-reduce:transition-none">Message</label>
-                                </div>
-                                <button type="submit"
-                                    className="inline-block w-full rounded bg-yellow-500 hover:bg-black px-6 pt-2.5 pb-2 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out">
-                                    Send
-                                </button>
-                            </form>
+
+                        <form className="bg-white grid gap-5" onSubmit={handleSubmit(onSubmit)}>
+                        <h3 className="text-lg font-bold leading-7 pb-1 border-b">
+                            Get In Touch
+                        </h3>
+                        <div className="md:flex w-full gap-5 items-center">
+                            <div className="md:w-1/2 flex flex-col">
+                                <label className="text-base font-semibold leading-none hidden">Name</label>
+                                <input {...register("name", { required: true })} tabIndex={0} arial-label="Your Name" type="name" className="text-sm leading-none p-3 focus:outline-none focus:border-[#34A777] bg-transparent border rounded border-[#F0F0F0] text-[#777777] placeholder:text-[#777777]" placeholder="Your Name" />
+                                {errors.name && <span className='text-xs text-red-500'>This field is required</span>}
+                            </div>
+                            <div className="md:w-1/2 flex flex-col md:mt-0 mt-7">
+                                <label className="text-base font-semibold leading-none hidden">Email</label>
+                                <input {...register("email", { required: true })} type="email" className="text-sm leading-none p-3 focus:outline-none focus:border-[#34A777] bg-transparent border rounded border-[#F0F0F0] text-[#777777] placeholder:text-[#777777]" placeholder="E-mail address" />
+                                {errors.email && <span className='text-xs text-red-500'>This field is required</span>}
+                            </div>
+                        </div>
+                        <div className="md:flex w-full gap-5 items-center">
+                            <div className="md:w-1/2 flex flex-col">
+                                <label className="text-base font-semibold leading-none hidden">Telephone</label>
+                                <input {...register("phone")} type="tel" className="text-sm leading-none p-3 focus:outline-none focus:border-[#34A777] bg-transparent border rounded border-[#F0F0F0] text-[#777777] placeholder:text-[#777777]" placeholder="Telephone" />
+                                {errors.phone && <span className='text-xs text-red-500'>This field is required</span>}
+                            </div>
+                            <div className="md:w-1/2 flex flex-col md:mt-0 mt-7">
+
+                                <label className="text-base font-semibold leading-none hidden">Subject</label>
+                                <input {...register("subject", { required: true })} type="text" className="text-sm leading-none p-3 focus:outline-none focus:border-[#34A777] bg-transparent border rounded border-[#F0F0F0] text-[#777777] placeholder:text-[#777777]" placeholder="Subject" />
+                                {errors.subject && <span className='text-xs text-red-500'>This field is required</span>}
+                            </div>
+                        </div>
+                        <div className="w-full flex flex-col">
+                            <label className="text-base font-semibold leading-none hidden">Message</label>
+                            <textarea {...register("comment", { required: true })} tabIndex={0} aria-label="Write your message" role="textbox" className="text-sm leading-none p-3 focus:outline-none focus:border-[#34A777] bg-transparent border rounded border-[#F0F0F0] text-[#777777] placeholder:text-[#777777] resize-none h-32" defaultValue={"Write your message"} />
+                            {errors.comment && <span className='text-xs text-red-500'>This field is required</span>}
+                        </div>
+                        <div className="flex items-center justify-start w-full">
+                            <input type='submit' className="mt-5 text-base font-semibold leading-none text-white py-4 px-10 bg-[#89DBC4] hover:bg-[#34A777]" value={sending ? 'SENDING...' : `SUBMIT`} />
+                        </div>
+                    </form>
+                           
+                        
+
+
                         </div>
                     </div>
                 </div>
