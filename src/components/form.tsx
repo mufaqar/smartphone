12405fb99983d0540244ec2f1e8a9791bcfe9599
mufaqar <mufaqar@gmail.com>
2023-12-29 17:21:10 +0500

    updates

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -25,8 +25,8 @@ function Form() {
                                 duration: 1,
                             }}
                             className="md:w-1/2 w-full">
-                            <h3 className='md:text-4xl text-3xl font-bold uppercase text-black Raleway mb-7'>
-                                BOOK APPOINTMENT
+                            <h3 className='md:text-4xl text-3xl font-bold  text-black Raleway mb-7'>
+                            Request a Free Quote
                             </h3>
                             <form className='grid grid-cols-1 gap-5'>
                                 <div className='grid md:grid-cols-2 grid-cols-1 gap-5'>
@@ -82,7 +82,7 @@ function Form() {
                                     <label htmlFor="message"
                                         className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-black transition-all duration-200 ease-out -translate-y-[0.9rem] peer-focus:scale-[0.9] peer-focus:opacity-100 opacity-0 peer-focus:text-black peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none ">Message</label>
                                 </div>
-                                <button className={`text-lg Raleway bg-yellow-500 px-10 py-2.5 font-medium text-white hover:bg-black hover:-translate-y-4 rounded-none`}>
+                                <button className={`text-lg Raleway bg-yellow-500 px-10 py-2.5 font-medium text-white hover:bg-black rounded-none`}>
                                     Submit
                                 </button>
                             </form>
