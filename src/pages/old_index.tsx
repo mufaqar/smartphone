8ff8f815ef06a0d710dfe215654c260c2f893f1d
mufaqar <mufaqar@gmail.com>
2024-05-15 11:43:19 +0500

    updated home

---
+++ src/pages/old_index.tsx
@@ -0,0 +1,46 @@
+
+// import Hero from '@/components/hero/index'
+// import Services from '@/components/services'
+// import Facilities from '@/components/fscilities'
+// import Blog_Sec from '../components/blogs'
+// import SeoMeta from '@/components/seo'
+// import { services } from '../../sanity/lib/queries'
+// import { blogs } from '../../sanity/lib/queries'
+// import { client } from '../../sanity/lib/client'
+
+// export default function Home({ servicesdata, blogsdata }: any) {
+//   return (
+//     <>
+
+//       <SeoMeta title="Budget Computers and Kiwi Mobiles in Rotorua, New Zealand." description="Budget Computers  specialize in offering a wide range of budget-friendly computers, mobile phones, and related accessories" url="https://budgetrepaircenter.nz" />
+//       <Hero />
+//       <Services data={servicesdata} />
+//       <Facilities />
+//       <Blog_Sec data={blogsdata} />
+//     </>
+//   )
+// }
+
+// export async function getServerSideProps() {
+//   const servicesdata = await client.fetch(services);
+//   const blogsdata = await client.fetch(blogs);
+//   return {
+//     props: {
+//       servicesdata,
+//       blogsdata,
+//     },
+//   };
+// }
+
+import React from 'react'
+
+function old_index() {
+  return (
+    <div>old_index</div>
+  )
+}
+
+export default old_index
+
+
+
